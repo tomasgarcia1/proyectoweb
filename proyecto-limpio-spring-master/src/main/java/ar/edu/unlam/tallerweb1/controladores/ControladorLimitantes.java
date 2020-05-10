92d4461d04a52df79b9969bc3909b0054a9f6528
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-10 01:01:40 -0300

    Create ControladorLimitantes.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLimitantes.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorLimitantes {
+
+	@RequestMapping("/limitantes")
+	public ModelAndView irRegistro() {
+		ModelMap model = new ModelMap();
+		return new ModelAndView("limitantes", model);
+	}
+}
