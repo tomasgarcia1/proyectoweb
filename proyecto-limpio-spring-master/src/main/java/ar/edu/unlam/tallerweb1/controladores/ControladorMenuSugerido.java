ee74029e7c26bb4f86aa5611276e02e9de1ab82b
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-10 02:27:01 -0300

    Update ControladorMenuSugerido.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorMenuSugerido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorMenuSugerido.java
@@ -8,10 +8,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorMenuSugerido {
-
-	@RequestMapping("/menuSugerido")
+  
+	@RequestMapping(path = "/menuSugerido", method = RequestMethod.GET)
 	public ModelAndView irAMenuSugerido() {
 		ModelMap modelo = new ModelMap();
 		return new ModelAndView("menuSugerido", modelo);
 	}
 }
+
+
