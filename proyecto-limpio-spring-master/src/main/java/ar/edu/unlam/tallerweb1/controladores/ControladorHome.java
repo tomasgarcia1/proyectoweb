d193d27bff21da7803ced632127ba4efcfbb399e
dkspl <s3p10l@github.com>
2020-05-09 22:08:34 -0300

    Método irAHome() modificado

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorHome {
-
+	// Escucha la URL /home por GET, y redirige a una vista.
 	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public ModelAndView irAHome() {
 		ModelMap modelo = new ModelMap();
