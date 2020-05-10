5e21793049664df458dbd8ca615fea38c1becc5c
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-10 02:34:49 -0300

    Update ControladorPedido.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
@@ -3,14 +3,16 @@ package ar.edu.unlam.tallerweb1.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorPedido {
-	
-	@RequestMapping("/crearPedido")
+
+	@RequestMapping(path = "/crearPedido", method = RequestMethod.GET)
 	public ModelAndView crearPedido() {
 		ModelMap model = new ModelMap();
 		return new ModelAndView("crearpedido", model);
 	}
+
 }
