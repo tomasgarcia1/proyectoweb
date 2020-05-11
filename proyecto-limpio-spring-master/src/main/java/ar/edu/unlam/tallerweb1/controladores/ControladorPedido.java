d070d897b4af06330503b820ac9443bcecc62f01
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:33:27 -0300

    Update ControladorPedido.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
@@ -1,18 +1,33 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Pedido;
+import ar.edu.unlam.tallerweb1.servicios.ServicioPedido;
+
 @Controller
 public class ControladorPedido {
-
-	@RequestMapping(path = "/crearPedido", method = RequestMethod.GET)
+	@Inject
+	private ServicioPedido servicioPedido;
+	
+	@RequestMapping("/crearPedido")
 	public ModelAndView crearPedido() {
 		ModelMap model = new ModelMap();
-		return new ModelAndView("crearpedido", model);
+		Pedido nuevoPedido=new Pedido();
+		Long idGenerado = servicioPedido.crearPedido(nuevoPedido);
+		model.put("pedido", nuevoPedido);
+		return new ModelAndView("pedidoRealizado", model);
 	}
-
+	
+	@RequestMapping(path = "/menuSugerido", method = RequestMethod.GET)
+	public ModelAndView irAMenuSugerido() {
+		ModelMap modelo = new ModelMap();
+		return new ModelAndView("menuSugerido", modelo);
+	}
 }
