4a8c32592a4ed326a9a4e4cda636e475eb3c7f20
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:33:45 -0300

    Create ServicioUsuario.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUsuario.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+public interface ServicioUsuario {
+	Long registrarUsuario(Usuario usuario);
+	Boolean validarExistenciaEmail(String email);
+}
