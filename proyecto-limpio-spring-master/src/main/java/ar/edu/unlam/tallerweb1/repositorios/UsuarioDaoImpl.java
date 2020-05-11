96f2199e4dfb1d090e30d3046432cbc454168b5e
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:33:14 -0300

    Create UsuarioDaoImpl.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/repositorios/UsuarioDaoImpl.java
@@ -0,0 +1,37 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import javax.inject.Inject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+@Repository
+@Transactional
+public class UsuarioDaoImpl implements UsuarioDao {
+	@Inject
+	private SessionFactory sesion;
+	@Override
+	public Long registrarUsuario(Usuario usuario) {
+		Long idGenerado=(Long) sesion.getCurrentSession().save(usuario);
+		return idGenerado;
+	}
+	
+	@Override
+	public Boolean validarExistenciaEmail(String email) {
+
+		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que invoca a este metodo y se crea un criterio
+		// de busqueda de Usuario donde el email y password sean iguales a los del objeto recibido como parametro
+		// uniqueResult da error si se encuentran más de un resultado en la busqueda.
+		final Session session = sesion.getCurrentSession();
+		Usuario usuarioEncontrado = (Usuario) session.createCriteria(Usuario.class)
+				.add(Restrictions.eq("email", email)).uniqueResult();
+		if(usuarioEncontrado!=null)
+			return true;
+		return false;
+	}
+}
