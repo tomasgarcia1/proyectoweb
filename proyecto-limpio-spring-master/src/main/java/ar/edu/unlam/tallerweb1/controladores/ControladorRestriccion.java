2821910e9848dd2801a90c14391b5c9bb34d7981
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:34:44 -0300

    Create ControladorRestriccion.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRestriccion.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorRestriccion {
+	@RequestMapping("/restricciones")
+	public ModelAndView irRegistro() {
+		ModelMap model = new ModelMap();
+		return new ModelAndView("restricciones", model);
+	}
+}
