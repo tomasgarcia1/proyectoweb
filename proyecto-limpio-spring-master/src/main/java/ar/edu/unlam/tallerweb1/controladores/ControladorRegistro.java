25943da258d78d627821315502fefc62ace98780
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:33:37 -0300

    Update ControladorRegistro.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistro.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistro.java
@@ -3,15 +3,14 @@ package ar.edu.unlam.tallerweb1.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorRegistro {
-
-	@RequestMapping(path = "/registro", method = RequestMethod.GET)
+	
+	@RequestMapping("/registro")
 	public ModelAndView irRegistro() {
-		ModelMap model = new ModelMap();
-		return new ModelAndView("registro", model);
+		ModelMap model= new ModelMap();
+		return new ModelAndView("registro",model);
 	}
 }
