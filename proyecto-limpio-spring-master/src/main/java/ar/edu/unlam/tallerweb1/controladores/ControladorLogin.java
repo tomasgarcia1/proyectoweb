44d25f54ee53b4b76c988bd513adee10c47fb603
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-11 18:33:33 -0300

    Update ControladorLogin.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
@@ -2,63 +2,59 @@ package ar.edu.unlam.tallerweb1.controladores;
 
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
+import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ControladorLogin {
 
-	// La anotacion @Autowired indica a Spring que se debe utilizar el contructor
-	// como mecanismo de inyección de dependencias,
-	// es decir, qeue lo parametros del mismo deben ser un bean de spring y el
-	// framewrok automaticamente pasa como parametro
-	// el bean correspondiente, en este caso, un objeto de una clase que implemente
-	// la interface ServicioLogin,
-	// dicha clase debe estar anotada como @Service o @Repository y debe estar en un
-	// paquete de los indicados en
+	// La anotacion @Autowired indica a Spring que se debe utilizar el contructor como mecanismo de inyección de dependencias,
+	// es decir, qeue lo parametros del mismo deben ser un bean de spring y el framewrok automaticamente pasa como parametro
+	// el bean correspondiente, en este caso, un objeto de una clase que implemente la interface ServicioLogin,
+	// dicha clase debe estar anotada como @Service o @Repository y debe estar en un paquete de los indicados en
 	// applicationContext.xml
 	private ServicioLogin servicioLogin;
+	@Inject
+	private ServicioUsuario servicioUsuario;
 
 	@Autowired
-	public ControladorLogin(ServicioLogin servicioLogin) {
+	public ControladorLogin(ServicioLogin servicioLogin){
 		this.servicioLogin = servicioLogin;
 	}
 
-	// Este metodo escucha la URL localhost:8080/NOMBRE_APP/login si la misma es
-	// invocada por metodo http GET
+	// Este metodo escucha la URL localhost:8080/NOMBRE_APP/login si la misma es invocada por metodo http GET
 	@RequestMapping("/login")
 	public ModelAndView irALogin() {
 
 		ModelMap modelo = new ModelMap();
-		// Se agrega al modelo un objeto del tipo Usuario con key 'usuario' para que el
-		// mismo sea asociado
+		// Se agrega al modelo un objeto del tipo Usuario con key 'usuario' para que el mismo sea asociado
 		// al model attribute del form que esta definido en la vista 'login'
 		Usuario usuario = new Usuario();
 		modelo.put("usuario", usuario);
-		// Se va a la vista login (el nombre completo de la lista se resuelve utilizando
-		// el view resolver definido en el archivo spring-servlet.xml)
-		// y se envian los datos a la misma dentro del modelo
+		// Se va a la vista login (el nombre completo de la lista se resuelve utilizando el view resolver definido en el archivo spring-servlet.xml)
+		// y se envian los datos a la misma  dentro del modelo
 		return new ModelAndView("login", modelo);
 	}
 
-	// Este metodo escucha la URL validar-login siempre y cuando se invoque con
-	// metodo http POST
-	// El método recibe un objeto Usuario el que tiene los datos ingresados en el
-	// form correspondiente y se corresponde con el modelAttribute definido en el
+	// Este metodo escucha la URL validar-login siempre y cuando se invoque con metodo http POST
+	// El método recibe un objeto Usuario el que tiene los datos ingresados en el form correspondiente y se corresponde con el modelAttribute definido en el
 	// tag form:form
 	@RequestMapping(path = "/validar-login", method = RequestMethod.POST)
 	public ModelAndView validarLogin(@ModelAttribute("usuario") Usuario usuario, HttpServletRequest request) {
 		ModelMap model = new ModelMap();
 
-		// invoca el metodo consultarUsuario del servicio y hace un redirect a la URL
-		// /home, esto es, en lugar de enviar a una vista
+		// invoca el metodo consultarUsuario del servicio y hace un redirect a la URL /home, esto es, en lugar de enviar a una vista
 		// hace una llamada a otro action a través de la URL correspondiente a ésta
 		Usuario usuarioBuscado = servicioLogin.consultarUsuario(usuario);
 		if (usuarioBuscado != null) {
@@ -71,10 +67,30 @@ public class ControladorLogin {
 		return new ModelAndView("login", model);
 	}
 
-	// Escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la
-	// url /login directamente.
+	// Escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la url /login directamente.
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public ModelAndView inicio() {
 		return new ModelAndView("redirect:/login");
+	}
+	
+	/* Controlador para registro de usuario, se reciben por parametro e-mail, password y rol (momentaneamente).
+	 * 
+	 * Se crearon interfaces y clases de registro y guardado de usuarios en servicios y repositorios.
+	 */
+	@RequestMapping(path = "/registrar", method = RequestMethod.GET)
+	public ModelAndView registrarUsuario(@RequestParam(value="email", required=true) String email, @RequestParam(value="password", required=true) String password, @RequestParam(value="rol") String rol)
+	{
+		ModelMap modeloRegistro=new ModelMap();
+		Usuario nuevoUsuario=new Usuario();
+		if(!servicioUsuario.validarExistenciaEmail(email)) {
+			nuevoUsuario.setEmail(email);
+			nuevoUsuario.setPassword(password);
+			nuevoUsuario.setRol(rol);
+			Long idGenerado=servicioUsuario.registrarUsuario(nuevoUsuario);
+			modeloRegistro.put("user", nuevoUsuario);
+			return new ModelAndView("registroCorrecto", modeloRegistro);
+		}
+		modeloRegistro.put("email", email);
+		return new ModelAndView("errorRegistro",modeloRegistro);
 	}
 }
