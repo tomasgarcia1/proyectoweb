2be768b678e62e3876122c8fb3d17e5e8ea7e6f4
tomasgarcia1 <40326389+tomasgarcia1@users.noreply.github.com>
2020-05-13 19:37:53 -0300

    Crear, obtener por id y borrar de comida

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorComida.java
@@ -0,0 +1,40 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unlam.tallerweb1.modelo.Comida;
+import ar.edu.unlam.tallerweb1.modelo.Pedido;
+import ar.edu.unlam.tallerweb1.servicios.ServicioComida;
+import ar.edu.unlam.tallerweb1.servicios.ServicioPedido;
+
+@Controller
+public class ControladorComida {
+	@Inject
+	private ServicioComida servicioComida;
+	
+	@RequestMapping("/crearComida")
+	public ModelAndView crearComida(@RequestParam(value="nombre", required=true) String nombre,
+			@RequestParam(value="descripcion", required=true) String descripcion,
+			@RequestParam(value="calorias", required=true) Double calorias) {
+		ModelMap model = new ModelMap();
+		Comida comida=new Comida();
+		comida.setNombre(nombre);
+		comida.setDescripcion(descripcion);
+		comida.setCalorias(calorias);
+		Long idGenerado = servicioComida.crearComida(comida);
+		model.put("comida", comida);
+		return new ModelAndView("nuevaComida", model);
+	}	
+	@RequestMapping("/eliminarPorId")
+	public ModelAndView eliminarPorId(@RequestParam(value="id", required=true) Long id)
+		 {
+		servicioComida.borrar(servicioComida.obtenerPorId(id));
+		return new ModelAndView("comidaborrada");
+	}
+}
