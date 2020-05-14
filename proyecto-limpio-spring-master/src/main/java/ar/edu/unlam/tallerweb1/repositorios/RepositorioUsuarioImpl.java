a8db30d749e63b5b62231ce6286376cf9c67e10c
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:13:27 -0300

    Update RepositorioUsuarioImpl.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
@@ -15,26 +15,27 @@ import javax.inject.Inject;
 @Repository("repositorioUsuario")
 public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
-	// Como todo repositorio maneja acciones de persistencia, normalmente estará inyectado el session factory de hibernate
+	// Como todo repositorio maneja acciones de persistencia, normalmente estará
+	// inyectado el session factory de hibernate
 	// el mismo está difinido en el archivo hibernateContext.xml
 	private SessionFactory sessionFactory;
 
-    @Autowired
-	public RepositorioUsuarioImpl(SessionFactory sessionFactory){
+	@Autowired
+	public RepositorioUsuarioImpl(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
 
 	@Override
 	public Usuario consultarUsuario(Usuario usuario) {
 
-		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que invoca a este metodo y se crea un criterio
-		// de busqueda de Usuario donde el email y password sean iguales a los del objeto recibido como parametro
+		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que
+		// invoca a este metodo y se crea un criterio
+		// de busqueda de Usuario donde el email y password sean iguales a los del
+		// objeto recibido como parametro
 		// uniqueResult da error si se encuentran más de un resultado en la busqueda.
 		final Session session = sessionFactory.getCurrentSession();
-		return (Usuario) session.createCriteria(Usuario.class)
-				.add(Restrictions.eq("email", usuario.getEmail()))
-				.add(Restrictions.eq("password", usuario.getPassword()))
-				.uniqueResult();
+		return (Usuario) session.createCriteria(Usuario.class).add(Restrictions.eq("email", usuario.getEmail()))
+				.add(Restrictions.eq("password", usuario.getPassword())).uniqueResult();
 	}
 
 }
