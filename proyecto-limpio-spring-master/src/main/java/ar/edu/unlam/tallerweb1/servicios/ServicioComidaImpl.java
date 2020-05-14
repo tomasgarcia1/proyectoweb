ac444c9e2e55bc6fd43d1bb27efa36c0e2a64e36
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:11:29 -0300

    Update ServicioComidaImpl.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComidaImpl.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComidaImpl.java
@@ -10,21 +10,22 @@ import ar.edu.unlam.tallerweb1.repositorios.ComidaDao;
 
 @Service
 @Transactional
-public class ServicioComidaImpl implements ServicioComida{
+public class ServicioComidaImpl implements ServicioComida {
 	@Inject
 	private ComidaDao comidaDao;
+
 	@Override
 	public Long crearComida(Comida comida) {
 		return comidaDao.crearComida(comida);
 	}
+
 	@Override
 	public Comida obtenerPorId(Long id) {
 		return comidaDao.ObtenerPorId(id);
 	}
+
 	public void borrar(Comida comida) {
 		comidaDao.borrar(comida);
 	}
 
 }
-}
-
