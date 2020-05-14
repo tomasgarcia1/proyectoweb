81a04533a428edc111a0bce5db01a012f65c2e85
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:13:55 -0300

    Update ServicioComida.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComida.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComida.java
@@ -5,11 +5,9 @@ import ar.edu.unlam.tallerweb1.modelo.Comida;
 public interface ServicioComida {
 
 	Long crearComida(Comida comida);
+
 	Comida obtenerPorId(Long id);
+
 	void borrar(Comida comida);
 
-
 }
-
-}
-
