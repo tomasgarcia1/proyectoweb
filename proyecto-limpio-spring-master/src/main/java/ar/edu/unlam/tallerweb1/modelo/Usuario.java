c965373ccc0f9930db2f35bda5116bdcadbf382e
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:12:44 -0300

    Update Usuario.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -13,35 +13,43 @@ import javax.persistence.Id;
 @Entity
 public class Usuario {
 
-	// La anotacion id indica que este atributo es el utilizado como clave primaria de la entity, se indica que el valor es autogenerado.
+	// La anotacion id indica que este atributo es el utilizado como clave primaria
+	// de la entity, se indica que el valor es autogenerado.
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	// para el resto de los atributo no se usan anotaciones entonces se usa el default de hibernate: la columna se llama igual que
-	// el atributo, la misma admite nulos, y el tipo de dato se deduce del tipo de dato de java.
+	// para el resto de los atributo no se usan anotaciones entonces se usa el
+	// default de hibernate: la columna se llama igual que
+	// el atributo, la misma admite nulos, y el tipo de dato se deduce del tipo de
+	// dato de java.
 	private String email;
 	private String password;
 	private Date fechaDeNacimiento;
-	//no se si lo de date esta bien
+	// no se si lo de date esta bien
 	private Integer altura;
 	private Double peso;
 	private Actividad actividad;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -49,24 +57,31 @@ public class Usuario {
 	public Integer getAltura() {
 		return altura;
 	}
+
 	public void setAltura(Integer altura) {
 		this.altura = altura;
 	}
+
 	public Actividad getActividad() {
 		return actividad;
 	}
+
 	public void setActividad(Actividad actividad) {
 		this.actividad = actividad;
 	}
+
 	public Date getFechaDeNacimiento() {
 		return fechaDeNacimiento;
 	}
+
 	public void setFechaDeNacimiento(Date fechaDeNacimiento) {
 		this.fechaDeNacimiento = fechaDeNacimiento;
 	}
+
 	public Double getPeso() {
 		return peso;
 	}
+
 	public void setPeso(Double peso) {
 		this.peso = peso;
 	}
