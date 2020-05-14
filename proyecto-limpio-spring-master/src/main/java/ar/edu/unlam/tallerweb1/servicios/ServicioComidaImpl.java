644fa05d0bd67cb569f48dbe648079d81d5c7f74
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 09:34:58 -0300

    Create ServicioComidaImpl.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComidaImpl.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.modelo.Comida;
+import ar.edu.unlam.tallerweb1.repositorios.ComidaDao;
+
+@Service
+@Transactional
+public class ServicioComidaImpl implements ServicioComida{
+	@Inject
+	private ComidaDao comidaDao;
+	@Override
+	public Long crearComida(Comida comida) {
+		return comidaDao.crearComida(comida);
+	}
+	@Override
+	public Comida obtenerPorId(Long id) {
+		return comidaDao.ObtenerPorId(id);
+	}
+	public void borrar(Comida comida) {
+		comidaDao.borrar(comida);
+	}
+}
