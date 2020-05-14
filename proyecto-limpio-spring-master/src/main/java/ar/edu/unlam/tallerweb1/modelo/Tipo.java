63a075034bb2835203c615debe2a0f47765701b8
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:12:52 -0300

    Update Tipo.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Tipo.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Tipo.java
@@ -11,16 +11,19 @@ public class Tipo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
