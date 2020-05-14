9e9d045fbbab8dfa6fc8251d00fd912b3942f4a7
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 09:35:20 -0300

    Create ServicioRestriccion.java

---
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioRestriccion.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import ar.edu.unlam.tallerweb1.modelo.Restriccion;
+
+public interface ServicioRestriccion {
+	Long crearRestriccion(Restriccion restriccion);
+	Restriccion obtenerRestriccionPorId(Long id);
+	void borrarRestriccion(Restriccion restriccion);
+}
