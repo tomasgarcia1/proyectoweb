178672953412d28b2e4f0b8a8c84ce046ef64ac9
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:11:42 -0300

    Update ServicioUsuario.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUsuario.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioUsuario.java
@@ -4,10 +4,10 @@ import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 public interface ServicioUsuario {
 	Long registrarUsuario(Usuario usuario);
+
 	Boolean validarExistenciaEmail(String email);
+
 	Boolean validarFormatoEmail(String email);
+
 	String encriptarPassword(String password);
 }
-
-}
-
