853f3ad466612b991b25c90e659a1b5b2f524e27
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:13:46 -0300

    Update UsuarioDao.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/repositorios/UsuarioDao.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/repositorios/UsuarioDao.java
@@ -4,9 +4,7 @@ import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 public interface UsuarioDao {
 	Long registrarUsuario(Usuario usuario);
+
 	Boolean validarExistenciaEmail(String email);
 
 }
-
-}
-
