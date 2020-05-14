99bdb610f055d358ee32e163205c4c5e62537560
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-14 10:12:06 -0300

    Update Comida.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Comida.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Comida.java
@@ -12,32 +12,38 @@ public class Comida {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre, descripcion;
-	private Double calorias;	
-	
+	private Double calorias;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public Double getCalorias() {
 		return calorias;
 	}
+
 	public void setCalorias(Double calorias) {
 		this.calorias = calorias;
 	}
-	
-	
+
 }
