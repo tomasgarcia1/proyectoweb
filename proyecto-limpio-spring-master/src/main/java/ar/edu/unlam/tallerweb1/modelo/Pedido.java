ce98f35f0857a8234ae296fdd683c6d229f430ee
seredonato <54689539+seredonato@users.noreply.github.com>
2020-05-25 20:14:04 -0300

    Update Pedido.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Pedido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/modelo/Pedido.java
@@ -1,42 +1,5 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Pedido {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	// private Datetime fecha;
-	private Estado estado;
-	private Usuario usuario;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Estado getEstado() {
-		return estado;
-	}
-
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
 
 }
