978bc95f14116817b1e78ca75dad8a4036777e2b
seredonato <54689539+seredonato@users.noreply.github.com>
2020-06-01 23:40:12 -0300

    Update ServicioComida.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComida.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComida.java
@@ -1,6 +1,9 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import java.util.List;
+
 import ar.edu.unlam.tallerweb1.modelo.Comida;
+import ar.edu.unlam.tallerweb1.modelo.Restriccion;
 
 public interface ServicioComida {
 
@@ -12,5 +15,9 @@ public interface ServicioComida {
 	Comida sugerirDesayunoPorCalorias(Double caloriasDiarias);
 	Comida sugerirAlmuerzoPorCalorias(Double caloriasDiarias);
 	Comida sugerirCenaPorCalorias(Double caloriasDiarias);
+
+	Comida sugerirDesayunoPorRestricciones(List<Restriccion> r);
+	Comida sugerirAlmuerzoPorRestricciones(List<Restriccion> r);
+	Comida sugerirCenaPorRestricciones(List<Restriccion> r);
 
 }
