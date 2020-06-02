de50448955062b5aff99fa53a0320f8e901fda12
seredonato <54689539+seredonato@users.noreply.github.com>
2020-06-01 23:40:01 -0300

    Update ServicioComidaImpl.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComidaImpl.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComidaImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.unlam.tallerweb1.modelo.Comida;
+import ar.edu.unlam.tallerweb1.modelo.Restriccion;
 import ar.edu.unlam.tallerweb1.repositorios.ComidaDao;
 
 @Service
@@ -50,6 +51,24 @@ public class ServicioComidaImpl implements ServicioComida {
 	public Comida sugerirCenaPorCalorias(Double caloriasDiarias) {
 		Double caloriasCena = caloriasDiarias*0.20;
 		List<Comida> cena = comidaDao.obtenerComidasSegunCalorias(caloriasCena);
+		Integer numeroRandom = (int) (Math.random()*cena.size());
+		return cena.get(numeroRandom);
+	}
+
+	public Comida sugerirDesayunoPorRestricciones(List<Restriccion> r) {
+		List<Comida> desayuno = comidaDao.obtenerComidasSegunRestricciones(r);
+		Integer numeroRandom = (int) (Math.random()*desayuno.size());
+		return desayuno.get(numeroRandom);
+	}
+	
+	public Comida sugerirAlmuerzoPorRestricciones(List<Restriccion> r) {
+		List<Comida> almuerzo = comidaDao.obtenerComidasSegunRestricciones(r);
+		Integer numeroRandom = (int) (Math.random()*almuerzo.size());
+		return almuerzo.get(numeroRandom);
+	}
+	
+	public Comida sugerirCenaPorRestricciones(List<Restriccion> r) {
+		List<Comida> cena = comidaDao.obtenerComidasSegunRestricciones(r);
 		Integer numeroRandom = (int) (Math.random()*cena.size());
 		return cena.get(numeroRandom);
 	}
