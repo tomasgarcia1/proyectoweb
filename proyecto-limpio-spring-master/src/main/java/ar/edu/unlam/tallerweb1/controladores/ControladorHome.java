805c977c6b345c6c0f29d1ae7921d2d78ab2c9ae
dkspl <s3p10l@github.com>
2020-06-03 02:24:24 -0300

    Revert "Merge remote-tracking branch 'origin/ailux' into sl4y3rd34d"
    This reverts commit 14c8a83378c71724ec271f71ef76cec7e92e0412, reversing
    changes made to 60d78aa213cba0169fce29457977af6a61c51840.

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
@@ -14,11 +14,5 @@ public class ControladorHome {
 		ModelMap modelo = new ModelMap();
 		return new ModelAndView ("home", modelo);
 	}
-	
-	@RequestMapping(path = "/interno")
-	public ModelAndView interno() {
-		ModelMap modelo = new ModelMap();
-		return new ModelAndView ("interno", modelo);
-	}
 }
 
