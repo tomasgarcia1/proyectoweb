7608fbb04003f60697acc3f2fed030d226a755ee
dkspl <s3p10l@github.com>
2020-06-06 16:50:22 -0300

    Cambio en funcionamiento de mostrar opciones

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPedido.java
@@ -1,5 +1,7 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,9 +10,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Comida;
 import ar.edu.unlam.tallerweb1.modelo.Estado;
 import ar.edu.unlam.tallerweb1.modelo.Pedido;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
@@ -23,79 +25,99 @@ public class ControladorPedido {
 	
 	/*
 	 * Se recibe el usuario activo en la sesion para obtener sus restricciones mediante su ID.
-	 * Se generan tres pedidos con la funcion generarPedidoPorRestricciones, tomando en cuenta las restricciones del usuario.
-	 * Cada pedido representa una opcion que puede elegir el cliente, solo puede elegir uno.
-	 * Estos pedidos se almacenan en la sesion actual bajo la key correspondiente al pedido, esto se hace
-	 * para persistir el pedido entre las vistas.
-	 * Se insertan en el modelo los pedidos para ser mostrados en la vista menuSugerido.
+	 * Se generan tres listas de comida con la funcion generarComidasPorRestricciones, 
+	 * tomando en cuenta las restricciones del usuario.
+	 * Cada lista representa una opcion de pedido que puede elegir el cliente, solo puede elegir una.
+	 * Se genera un String con los ID de las comidas que tiene cada lista.
+	 * Se insertan en el modelo las listas para ser mostrados en la vista menuSugerido,
+	 * y los Strings se mandan para ser usados como valores en el formulario.
+	 * La vista de retorno es el menu con las 3 opciones de sugerencias.
 	 */
 	@RequestMapping(path = "/menuSugerido")
 	public ModelAndView irAMenuSugerido(HttpServletRequest request) {
 		Usuario user=(Usuario)request.getSession().getAttribute("usuario");
 		ModelMap model = new ModelMap();
-		Pedido pedido1=servicioPedido.generarPedidoPorRestricciones(user.getId());
-		Pedido pedido2=servicioPedido.generarPedidoPorRestricciones(user.getId());
-		Pedido pedido3=servicioPedido.generarPedidoPorRestricciones(user.getId());
-		//HttpSession session = request.getSession(true);
-		request.getSession().setAttribute("pedido1", pedido1);
-		request.getSession().setAttribute("pedido2", pedido2);
-		request.getSession().setAttribute("pedido3", pedido3);
-		model.put("pedido1", pedido1);
-		model.put("pedido2", pedido2);
-		model.put("pedido3", pedido3);
+		
+		List<Comida> opcion1=servicioPedido.generarComidasPorRestricciones(user.getId());
+		List<Comida> opcion2=servicioPedido.generarComidasPorRestricciones(user.getId());
+		List<Comida> opcion3=servicioPedido.generarComidasPorRestricciones(user.getId());
+		String idComidas1=servicioPedido.concatenarIdComidas(opcion1);
+		String idComidas2=servicioPedido.concatenarIdComidas(opcion2);
+		String idComidas3=servicioPedido.concatenarIdComidas(opcion3);
+		
+		model.put("comidas1", opcion1);
+		model.put("comidas2", opcion2);
+		model.put("comidas3", opcion3);
+		model.put("idcomidas1", idComidas1);
+		model.put("idcomidas2", idComidas2);
+		model.put("idcomidas3", idComidas3);
 		return new ModelAndView("menuSugerido", model);
 	}
-
 	/*
-	 * Se recibe como parametro un string con la opcion elegida en la vista menuSugerido.
-	 * Esta opcion es evaluada para asignarle a la variable nuevoPedido el pedido acorde a lo elegido.
+	 * Tiene el mismo funcionamiento que irAMenuSugerido, con la diferencia de que usa el metodo generarComidasPorCalorias,
+	 * donde se recibe como parametro el usuario para obtener las calorias diarias.
+	 */
+	@RequestMapping(path = "/menuCalorias")
+	public ModelAndView irAMenuCalorias(HttpServletRequest request) {
+		Usuario user=(Usuario)request.getSession().getAttribute("usuario");
+		ModelMap model = new ModelMap();
+		
+		List<Comida> opcion1=servicioPedido.generarComidasPorCalorias(user);
+		List<Comida> opcion2=servicioPedido.generarComidasPorCalorias(user);
+		List<Comida> opcion3=servicioPedido.generarComidasPorCalorias(user);
+		String idComidas1=servicioPedido.concatenarIdComidas(opcion1);
+		String idComidas2=servicioPedido.concatenarIdComidas(opcion2);
+		String idComidas3=servicioPedido.concatenarIdComidas(opcion3);
+		
+		model.put("comidas1", opcion1);
+		model.put("comidas2", opcion2);
+		model.put("comidas3", opcion3);
+		model.put("idcomidas1", idComidas1);
+		model.put("idcomidas2", idComidas2);
+		model.put("idcomidas3", idComidas3);
+		return new ModelAndView("menuCalorias", model);
+	}
+	/*
+	 * Se recibe como parametro un String con la opcion elegida en la vista menuSugerido.
+	 * Esta opcion es mandada como parametro del metodo generarPedidoPorIdComidas,
+	 * que se encarga de generar un pedido que contiene las comidas que eligio el usuario.
 	 * Se asigna el usuario que esta activo en la sesion al pedido.
 	 * Se cambia el estado del pedido.
 	 * Se agrega el pedido a la base de datos.
 	 * Se agrega al model los datos del pedido para mostrarlos en pantalla como vista previa.
 	 */
-	@RequestMapping(path="/generarpedido", method=RequestMethod.GET)
-	public ModelAndView vistaPedido(@RequestParam(value="pedido", required=true)
-	String pedido, HttpServletRequest request) {
+	@RequestMapping(path="/generarpedido", method=RequestMethod.POST)
+	public ModelAndView vistaPedido(@ModelAttribute("idComidas") String idComidas, HttpServletRequest request) {
 		Usuario user=(Usuario)request.getSession().getAttribute("usuario");
 		ModelMap model = new ModelMap();
 		Pedido nuevoPedido=new Pedido();
-		switch(pedido)
-		{
-			case "1":	nuevoPedido=(Pedido)request.getSession().getAttribute("pedido1");
-					break;
-			case "2":	nuevoPedido=(Pedido)request.getSession().getAttribute("pedido2");
-					break;
-			case "3":	nuevoPedido=(Pedido)request.getSession().getAttribute("pedido3");
-					break;
-		}
+		
+		nuevoPedido=servicioPedido.generarPedidoPorIdComidas(idComidas);
 		nuevoPedido.setUsuario(user);
 		nuevoPedido.setEstado(Estado.ACEPTADO);
 		Long idPedido=servicioPedido.crearPedido(nuevoPedido);
+		
 		model.put("id", idPedido);
 		model.put("precio", nuevoPedido.getPrecio());
 		model.put("comidas", nuevoPedido.getComidas());
 		model.put("pedido", nuevoPedido);
-		request.getSession().removeAttribute("pedido1");
-		request.getSession().removeAttribute("pedido2");
-		request.getSession().removeAttribute("pedido3");
 		return new ModelAndView("pedidoPorConfirmar", model);
 	}
-	
 	/*
 	 * Se le envia el pedido creado y seteado anteriormente, y se le otorga el estado de "PAGADO".
 	 * Se muestra por pantalla el numero de pedido, dado por el ID generado en generarPedido().
 	 */
 	@RequestMapping(path="/pagarpedido", method=RequestMethod.POST)
-	public ModelAndView pagarPedido(@ModelAttribute("pedido") Pedido pedido, HttpServletRequest request) {
+	public ModelAndView pagarPedido(@ModelAttribute("pedido") Pedido pedido , HttpServletRequest request) {
 		ModelMap model = new ModelMap();
-		Pedido pedido2=servicioPedido.buscarPedidoPorId(pedido.getId());
-		pedido2.setEstado(Estado.PROCESO);
-		servicioPedido.actualizarPedido(pedido2);
-		model.put("pedido", pedido2);
-		return new ModelAndView("pedidoConfirmado", model);
+		
+		Pedido nuevoPedido=servicioPedido.buscarPedidoPorId(pedido.getId());
+		nuevoPedido.setEstado(Estado.PROCESO);
+		servicioPedido.actualizarPedido(nuevoPedido);
+		
+		model.put("pedido", nuevoPedido);
+		return new ModelAndView("pedidoRealizado", model);
 	}
-	
 	/*
 	 * Se recibe por parametro el ID del pedido que queremos cancelar.
 	 * Si es distinto de null, se realiza la accion sobre el pedido existente.
@@ -108,21 +130,7 @@ public class ControladorPedido {
 		return new ModelAndView("redirect:/home");
 	}
 	
-	@RequestMapping(path = "/menuCalorias")
-	public ModelAndView irAMenuCalorias(HttpServletRequest request) {
-		Usuario user=(Usuario)request.getSession().getAttribute("usuario");
-		ModelMap model = new ModelMap();
-		Pedido pedido1=servicioPedido.generarPedidoPorCalorias(user);
-		Pedido pedido2=servicioPedido.generarPedidoPorCalorias(user);
-		Pedido pedido3=servicioPedido.generarPedidoPorCalorias(user);
-		request.getSession().setAttribute("pedido1", pedido1);
-		request.getSession().setAttribute("pedido2", pedido2);
-		request.getSession().setAttribute("pedido3", pedido3);
-		model.put("pedido1", pedido1);
-		model.put("pedido2", pedido2);
-		model.put("pedido3", pedido3);
-		return new ModelAndView("menuCalorias", model);
-	}
+	
 	/*@RequestMapping(path="/mispedidos")
 	public ModelAndView listarPedidos(HttpServletRequest request) {
 		ModelMap model = new ModelMap();
--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPedido.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPedido.java
@@ -1,14 +1,19 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Comida;
 import ar.edu.unlam.tallerweb1.modelo.Pedido;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
 public interface ServicioPedido {
 	Long crearPedido(Pedido pedido);
-	Pedido generarPedidoPorRestricciones(Long id);
-	Pedido generarPedidoPorCalorias(Usuario usuario);
+	List<Comida> generarComidasPorRestricciones(Long id);
+	List<Comida> generarComidasPorCalorias(Usuario usuario);
 	Double calcularImporteTotal(Pedido pedido);
 	void cancelarPedido(Long id);
 	Pedido buscarPedidoPorId(Long id);
 	void actualizarPedido(Pedido pedido);
+	String concatenarIdComidas(List<Comida> comidas);
+	Pedido generarPedidoPorIdComidas(String idComidas);
 }
