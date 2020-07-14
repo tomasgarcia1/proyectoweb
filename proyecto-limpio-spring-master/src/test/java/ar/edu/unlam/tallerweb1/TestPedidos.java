90df43e4343a586b4f85513e7276e1c95d59a4e2
dkspl <s3p10l@github.com>
2020-07-14 20:56:38 -0300

    Algunos tests ServicioPedido y ControladorPedido
    Test de actualizar, calcular importe y concatenar comidas.

--- proyecto-limpio-spring-master/src/test/java/ar/edu/unlam/tallerweb1/TestPedidos.java
+++ proyecto-limpio-spring-master/src/test/java/ar/edu/unlam/tallerweb1/TestPedidos.java
@@ -2,6 +2,9 @@ package ar.edu.unlam.tallerweb1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,9 +12,15 @@ import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unlam.tallerweb1.controladores.ControladorPedido;
+import ar.edu.unlam.tallerweb1.modelo.Comida;
+import ar.edu.unlam.tallerweb1.modelo.Estado;
+import ar.edu.unlam.tallerweb1.modelo.Pedido;
 import ar.edu.unlam.tallerweb1.modelo.Rol;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioComida;
+import ar.edu.unlam.tallerweb1.servicios.ServicioComidaImpl;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPedido;
+import ar.edu.unlam.tallerweb1.servicios.ServicioPedidoImpl;
 
 public class TestPedidos {
 	@Test
@@ -51,5 +60,79 @@ public class TestPedidos {
 		assertThat(model.getViewName()).isEqualTo("redirect:/mispedidos");
 		verify(servicioPedido, times(0)).listarPedidos();
 	}
-
+	@Test
+	public void testConcatenacionCorrecta()
+	{
+		ServicioPedido sp = new ServicioPedidoImpl();
+		Comida c1=new Comida();
+		Comida c2=new Comida();
+		Comida c3=new Comida();
+		List<Comida> lista=new ArrayList<Comida>();
+		
+		c1.setId((long)1);
+		c2.setId((long)4);
+		c3.setId((long)8);
+		lista.add(c1);
+		lista.add(c2);
+		lista.add(c3);
+		
+		assertThat(sp.concatenarIdComidas(lista)).isEqualTo("1,4,8");
+		
+	}
+	@Test
+	public void testImporteTotal()
+	{
+		ServicioPedido sp = new ServicioPedidoImpl();
+		Comida c1=new Comida();
+		Comida c2=new Comida();
+		Comida c3=new Comida();
+		List<Comida> lista=new ArrayList<Comida>();
+		Double envio=15.0;
+		
+		c1.setPrecio(30.0);
+		c2.setPrecio(60.0);
+		c3.setPrecio(390.0);
+		
+		lista.add(c1);
+		lista.add(c2);
+		lista.add(c3);
+		
+		assertThat(sp.calcularImporteTotal(lista, envio)).isEqualTo(495.0);
+		
+	}
+	@Test
+	public void testActualizarPedido()
+	{
+		HttpServletRequest request=mock(HttpServletRequest.class);
+		ServicioPedido servicioPedido=mock(ServicioPedido.class);
+		ControladorPedido controlador=new ControladorPedido();
+		controlador.setServicioPedido(servicioPedido);
+		Pedido pedido=mock(Pedido.class);
+		String id="1";
+		String estado="PROCESO";
+		Estado estadoPedido=Enum.valueOf(Estado.class, estado);
+		
+		when(servicioPedido.buscarPedidoPorId(Long.parseLong(id))).thenReturn(pedido);
+		ModelAndView model=controlador.actualizarEstado(id, estado, request);
+		
+		assertThat(model.getViewName()).isEqualTo("redirect:/verpedidos");
+		
+		verify(servicioPedido, times(1)).actualizarPedido(pedido, estadoPedido);
+	}
+	/*@Test 
+	public void testComidasPorRestricciones()
+	{
+		ServicioPedido sp = new ServicioPedidoImpl();
+		ServicioComida sc = mock(ServicioComida.class);
+		Long id=(long) 1;
+		Comida desayuno=mock(Comida.class);
+		Comida almuerzo=mock(Comida.class);
+		Comida cena=mock(Comida.class);
+		
+		when(sc.sugerirDesayunoPorRestricciones(id)).thenReturn(desayuno);
+		when(sc.sugerirAlmuerzoPorRestricciones(id)).thenReturn(almuerzo);
+		when(sc.sugerirCenaPorRestricciones(id)).thenReturn(cena);
+		
+		
+	}*/
 }
